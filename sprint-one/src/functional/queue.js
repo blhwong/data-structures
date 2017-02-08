8e738ef1c06d4eacd83a474751cdb636ac00798c
blhwong <b.lh.wong@gmail.com>
2017-02-08 15:37:57 -0800

    Implement Queue-specific Behavior

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,28 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
+  var start = 0;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    //Set the value
+    storage[start + size] = value; 
+
+    //Increment size
     size++;
+
+
   };
 
   someInstance.dequeue = function() {
-    size--;
+    if (size > 0) { 
+      var returnValue = storage[start];
+      start++;
+      size--;
+
+      return returnValue;
+    }
+
   };
 
   someInstance.size = function() {
