5d5f27bcb5786a5c0367d22b425aa45f9500b7b9
blhwong <b.lh.wong@gmail.com>
2017-02-08 15:02:24 -0800

    Pass all stack tests in functional class

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,10 +6,15 @@ var Stack = function() {
   var size = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
     size++;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      size--;
+      return storage[size];
+    }
   };
 
   someInstance.size = function() {
