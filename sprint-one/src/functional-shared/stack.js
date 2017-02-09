8eff85d4a19925af4b48d65c6c2cf63d9d6b7ea5
blhwong <b.lh.wong@gmail.com>
2017-02-08 16:02:28 -0800

    Implement Functional Shared Stack Behavior

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,62 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {stackSize: 0, storage: {}};
+
+
+  //extend the stack;
+  _.extend(stack, stackMethods);
+
+  return stack;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  pop: function () {
+    if (this.stackSize > 0) {
+      this.stackSize--;
+      return this.storage[this.stackSize];
+    }
+  },
+
+  size: function() {
+    return this.stackSize;
+  }
+
+};
 
 
+
+
+/*
+var Stack = function() {
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var size = 0;
+  // Implement the methods below
+    someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
+  };
+
+  someInstance.pop = function() {
+    if (size > 0) {
+      size--;
+      return storage[size];
+    }
+  };
+
+  someInstance.size = function() {
+    return size;
+  };
+
+  return someInstance;
+};
+*/
