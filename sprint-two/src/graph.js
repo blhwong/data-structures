0544cf94cf374167de10aba13bf281b988289ae0
blhwong <b.lh.wong@gmail.com>
2017-02-09 10:54:07 -0800

    Implement Add functionality for Graphs

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,14 +2,21 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.graph = {};
+
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.graph[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (node in this.graph) {
+    return true;
+  }
+  return false;
 };
 
 // Removes a node from the graph.
