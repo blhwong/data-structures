c4618df091c902a756b3a5a297f099e966fd2856
blhwong <b.lh.wong@gmail.com>
2017-02-08 20:21:17 -0800

    Pass test case for remove head

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,45 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+  var size = 0;
   list.addToTail = function(value) {
+    // if (size === 0) {
+    //   list.head = Node(value);
+    // }
+    // size++;
+    // list.tail = Node(value);
+
+    // // //If it's currently empty
+    // if (size === 0) {
+    //   var newNode = Node(value);
+    //   list.head = newNode;
+    //   list.tail = newNode;
+    // } else if (size === 1) { //If it's not empty
+    //   var newNode = Node(value);
+    //   list.tail = newNode;
+
+    //   list.head.next = newNode;
+    
+    // } else {
+    //   list.tail.next = Node(value);
+    // }
+
+    if (size === 0) {
+      var newNode = Node(value);
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      var newNode = Node(value);
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+
+    size++;
   };
 
   list.removeHead = function() {
+    size --;
+    list.head = list.head.next;
   };
 
   list.contains = function(target) {
