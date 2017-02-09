e84b30431fb0172fb83ab4a29641310fc893c6a1
blhwong <b.lh.wong@gmail.com>
2017-02-08 16:28:13 -0800

    Implement Protypal Stack & Queue Behavior

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,73 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.storage = {};
+  queue.start = 0;
+  queue.queueSize = 0;
+
+  return queue;
 };
 
-var queueMethods = {};
+
+var queueMethods = {
+  enqueue: function(value) {
+    //Set the value
+    this.storage[this.start + this.queueSize] = value; 
+
+    //Increment size
+    this.queueSize++;
+
+  },
+  dequeue: function() {
+    if (this.queueSize > 0) { 
+      var returnValue = this.storage[this.start];
+      this.start++;
+      this.queueSize--;
+
+      return returnValue;
+    }
+  },
+  size: function() {
+    return this.queueSize;
+  }
+};
+
+/*
 
 
+var Queue = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var obj = {queueSize: 0, start: 0, storage: {}};
+
+  _.extend(obj, queueMethods);
+
+  return obj;
+};
+
+var queueMethods = {
+  enqueue: function(value) {
+    //Set the value
+    this.storage[this.start + this.queueSize] = value; 
+
+    //Increment size
+    this.queueSize++;
+
+  },
+  dequeue: function() {
+    if (this.queueSize > 0) { 
+      var returnValue = this.storage[this.start];
+      this.start++;
+      this.queueSize--;
+
+      return returnValue;
+    }
+  },
+  size: function() {
+    return this.queueSize;
+  }
+};
+
+
+*/
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,68 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+
+  stack.stackSize = 0;
+  stack.storage = {};
+
+  return stack;
 };
 
-var stackMethods = {};
 
 
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  pop: function () {
+    if (this.stackSize > 0) {
+      this.stackSize--;
+      return this.storage[this.stackSize];
+    }
+  },
+
+  size: function() {
+    return this.stackSize;
+  }
+
+};
+
+
+
+/*
+var Stack = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var stack = {stackSize: 0, storage: {}};
+
+
+  //extend the stack;
+  _.extend(stack, stackMethods);
+
+  return stack;
+};
+
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  pop: function () {
+    if (this.stackSize > 0) {
+      this.stackSize--;
+      return this.storage[this.stackSize];
+    }
+  },
+
+  size: function() {
+    return this.stackSize;
+  }
+
+};
+
+
+*/
