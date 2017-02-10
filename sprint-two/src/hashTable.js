9fe509a86dca6630fe5a856879bb617b4be36174
blhwong <b.lh.wong@gmail.com>
2017-02-09 19:40:58 -0800

    Implement hash table functionality

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,48 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var arrayAtBucket = this._storage.get(index);
+
+  
+  if (arrayAtBucket) { //If there's items at the bucket [[1: apple]] --> [1: orange]
+    var toInsert = [k, v];
+    for (var a = 0; a < arrayAtBucket.length; a++) {
+      if (arrayAtBucket[a][0] === k ) { //
+        arrayAtBucket[a] = toInsert;
+        return;
+      }
+    }
+    arrayAtBucket.push(toInsert); 
+  } else { // It's empty
+    arrayAtBucket = [[k, v]];
+  }
+
+  this._storage.set(index, arrayAtBucket); 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  //expects undefined or [[k, v]]
+  var arrayAtBucket = this._storage.get(index);
+  for (var a = 0; a < arrayAtBucket.length; a++) {
+    if (arrayAtBucket[a][0] === k) {
+      return arrayAtBucket[a][1];
+    } 
+  }  
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var arrayAtBucket = this._storage.get(index);
+
+  for (var a = 0; a < arrayAtBucket.length; a++) {
+    if (arrayAtBucket[a][0] === k) {
+      arrayAtBucket = arrayAtBucket.splice(0, a).concat(arrayAtBucket.splice(a + 1));
+      this._storage.set(index, arrayAtBucket);
+    }
+  }
+
 };
 
 
