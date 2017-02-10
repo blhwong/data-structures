59fa7ec3eaecb762902b8399cbf24709f695ef75
blhwong <b.lh.wong@gmail.com>
2017-02-09 20:44:34 -0800

    Pass first test case for BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,47 @@
 var BinarySearchTree = function(value) {
+  var bst = {};
+  _.extend(bst, bstMethods);
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
+
+  return bst;
 };
 
+var bstMethods = {
+  insert: function(value, head) {
+    console.log(this.bst);
+    //If it's bigger than tree's value
+    head = head || this.bst;
+    if (head === undefined) {
+      this.bst = BinarySearchTree(value);
+      return;
+    }
+    
+    if (value > head.value) {
+      //If it's null you insert
+      if (head.right === null) { 
+        head.right = BinarySearchTree(value);
+      } else {
+        head.insert(value, head.right);
+      }
+
+    } else if (value < head.value) {
+      if (head.left === null) {
+        head.left = BinarySearchTree(value);  
+      } else {
+        head.insert(value, head.left);
+      }
+    }
+
+  },
+  contains: function() {
+
+  },
+  depthFirstLog: function() {
+
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
